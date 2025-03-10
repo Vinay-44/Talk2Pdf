9fcbdf14e4daaaabdc38d9365c31312d4c6b2c93
Vinay-44 <vinaymudgal441163@gmail.com>
2025-03-10 14:17:00 +0530

    fix

--- components/PDFViewer.tsx
+++ components/PDFViewer.tsx
@@ -3,10 +3,12 @@ import React from "react";
 type Props = { pdf_url: string };
 
 const PDFViewer = ({ pdf_url }: Props) => {
+  console.log(pdf_url);
+  
   return (
     <div className="h-full bg-[#161B22]/70 backdrop-blur-sm rounded-lg border border-[#00FF9D]/20">
       <iframe
-        src={`https://docs.google.com/gview?url=${pdf_url}&embedded=true`}
+        src={`${pdf_url}`}
         className="w-full h-full rounded-lg"
       ></iframe>
     </div>
